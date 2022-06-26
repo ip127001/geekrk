cc9a71aff4e78dc3483ac07c5780f311f212677f
Rohit Kumawat <rkumawat125@gmail.com>
2022-06-26 12:53:08 +0530

    added event tracking

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,46 @@
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
 
-function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>;
-}
+const ImageComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={300} // Desired size with correct aspect ratio
+    width={300} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
 
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to my blog</h1>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        onLoad={() => console.log("script loaded succesfully")}
+      ></Script>
 
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+    `}
+      </Script>
+      <Head>
+        <title>RK Blog</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <h1>Welcome to my blog</h1>
+      <ImageComponent />
       <h2>Read my blogs</h2>
-      <Link href="/posts/hoisting"><a>What is hoisting in javascript</a></Link>
+      <Link href="/posts/hoisting">
+        <a>What is hoisting in javascript</a>
+      </Link>
     </div>
   );
 }
--- pages/posts/hoisting.js
+++ pages/posts/hoisting.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Hoisting() {
   return (
     <div>
+      <Head>
+        <title>Hoisting</title>
+      </Head>
       <h2>
         <Link href="/">
           <a>Back to home</a>
