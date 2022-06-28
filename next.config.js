bbae0bb18adc3d11017e7fe24043c5feee87751c
Rohit Kumawat <rkumawat125@gmail.com>
2022-06-28 12:49:25 +0530

    next config added

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    USER_NAME: process.env.USER_NAME,
+    PASSWORD: process.env.PASSWORD,
+    CLUSTER_NAME: process.env.CLUSTER_NAME,
+    DB_NAME: process.env.DB_NAME,
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+  },
+};
